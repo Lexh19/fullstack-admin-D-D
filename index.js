add1a943276e8cc656effba127da01c03a6dd8d6
Lexh19 <iqbaalhzd@gmail.com>
2024-05-29 13:24:25 +0700

    adding vercel config

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from "express";
 import FileUpload from "express-fileupload";
 import cors from "cors";
 import ProductRoute from "./routes/ProductRoute.js";
+const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.use(cors());
 app.use(express.json());
@@ -11,4 +14,6 @@ app.use(FileUpload());
 app.use(express.static("public"));
 app.use(ProductRoute);
 
-app.listen(5000, ()=> console.log('Server Up and Running...'));
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
